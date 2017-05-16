881c877d562290eaab713f3ff8cf03038c88e878
egee-irl <brian@egee.io>
2017-05-16 07:15:47 -0700

    arrange requires, use connection factory

--- main.js
+++ main.js
@@ -3,7 +3,5 @@ if (process.argv[2] == null) {
   process.exit()
 }
 
-const DiscordJs = require('discord.js')
 const Androgee = require('./lib/androgee.js')
-
-new Androgee(new DiscordJs.Client(), process.argv[2])
+new Androgee().connect(process.argv[2])
