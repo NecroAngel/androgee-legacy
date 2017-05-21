b7a87ac52d5902723910837b723ba0ed3d70fddd
egee-irl <brian@egee.io>
2017-05-21 12:43:33 -0700

    WIP

--- main.js
+++ main.js
@@ -1,7 +1,2 @@
-if (process.argv[2] == null) {
-  console.log('You forgot to provide the Discord server token.')
-  process.exit()
-}
-
 const Androgee = require('./lib/androgee.js')
-new Androgee().connect(process.argv[2])
+new Androgee().bootstrap()
